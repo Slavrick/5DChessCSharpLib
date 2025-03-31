64ed186ac14f8650617970c9623262d27aba126b
Maverick <32693706+Slavrick@users.noreply.github.com>
2025-03-30 19:23:58 -0700

    initial Commit

---
+++ 5DChess/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+using TestRewrite;
+
+
+TurnTester.TestTurnEquals();
+CoordTester.TestAllCoordFiveFuncs();
+FENParserTest.TestMoveParser();
+FENParserTest.TestSANParser();
+FENParserTest.TestShadParser();
+FENParserTest.TestFENFileParser();
+FENParserTest.TestShadFEN();
+FENParserTest.TestAmbiguityInfoParser();
+MateTest.BenchmarkMates();
+GameStateTest.TestGameStateMutation();
